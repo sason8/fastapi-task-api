ebe74db340d778f728d8200b3e19ca3ded846cbc
sason8 <sason.panda2@gmail.com>
2026-06-05 04:31:22 +0200

    feat: setup database, sqlalchemy models and pydantic schemas

---
+++ models.py
@@ -0,0 +1,12 @@
+﻿from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    is_completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
---
+++ schemas.py
@@ -0,0 +1,21 @@
+﻿from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class TaskBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    is_completed: bool = False
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(TaskBase):
+    pass
+
+class Task(TaskBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
